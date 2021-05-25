cb39a845e63c2fbdcdd10abcd478204c8977236d
Colin <colinpeiffer6@gmail.com>
2021-05-25 23:31:49 +0200

    simple guild info

--- main.py
+++ main.py
@@ -538,36 +538,12 @@ async def userinfo(ctx, target: Optional[Member]):
 
 @client.command(aliases=["si", "gi", "guildinfo"])
 async def serverinfo(ctx):
-    embed = Embed(title="Server information",
-					  colour=0x90EE90,
-					  timestamp=datetime.datetime.utcnow())
-
+    embed = discord.Embed(title=ctx.guild.name, timestamp=datetime.datetime.utcnow(), color=discord.Color.blue())
     embed.set_thumbnail(url=ctx.guild.icon_url)
-
-    statuses = [len(list(filter(lambda m: str(m.status) == "online", ctx.guild.members))),
-                len(list(filter(lambda m: str(m.status) == "idle", ctx.guild.members))),
-                len(list(filter(lambda m: str(m.status) == "dnd", ctx.guild.members))),
-                len(list(filter(lambda m: str(m.status) == "offline", ctx.guild.members)))]
-
-    fields = [("ID", ctx.guild.id, True),
-                ("Owner", ctx.guild.owner, True),
-                ("Region", ctx.guild.region, True),
-                ("Created at", ctx.guild.created_at.strftime("%d/%m/%Y %H:%M:%S"), True),
-                ("Members", len(ctx.guild.members), True),
-                ("Humans", len(list(filter(lambda m: not m.bot, ctx.guild.members))), True),
-                ("Bots", len(list(filter(lambda m: m.bot, ctx.guild.members))), True),
-                ("Banned members", len(await ctx.guild.bans()), True),
-                ("Statuses", f"🟢 {statuses[0]} 🟠 {statuses[1]} 🔴 {statuses[2]} ⚪ {statuses[3]}", True),
-                ("Text channels", len(ctx.guild.text_channels), True),
-                ("Voice channels", len(ctx.guild.voice_channels), True),
-                ("Categories", len(ctx.guild.categories), True),
-                ("Roles", len(ctx.guild.roles), True),
-                ("Invites", len(await ctx.guild.invites()), True),
-                ("\u200b", "\u200b", True)]
-
-    for name, value, inline in fields:
-        embed.add_field(name=name, value=value, inline=inline)
-
+    embed.add_field(name="Server created at", value=ctx.guild.created_at)
+    embed.add_field(name="Server Owner", value=ctx.guild.owner)
+    embed.add_field(name="Server Region", value=ctx.guild.region)
+    embed.add_field(name="Server ID", value=ctx.guild.id)
     await ctx.send(embed=embed)
 
 
