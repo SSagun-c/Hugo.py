42ce8d8cbe2cdd8e7fdc7e46db3fbabb6e1112f2
Colin <colinpeiffer6@gmail.com>
2021-05-27 23:00:25 +0200

    hmmm

--- main.py
+++ main.py
@@ -454,44 +454,7 @@ REDDIT_APP_SECRET = os.getenv("RAS")
 USERNAME = os.getenv("user")
 PASSWORD = os.getenv("pass")
 
-rreddit = praw.Reddit(client_id = os.environ['RAI'],
-                    client_secret = os.environ['RAS'],
-                    username = os.environ['user'],
-                    password = os.environ['pass'],
-                    user_agent = 'SSagunPraw')
 
-@client.command()
-@cooldown(1, 8, commands.BucketType.guild)
-async def reddit(ctx, subred = "meme"):  # default subreddit is meme
-    subreddit = rreddit.subreddit(subred)
-    all_subs = []
-
-    top = subreddit.top(limit = 75)
-
-    for submission in top:
-        all_subs.append(submission)
-    
-    random_sub = random.choice(all_subs)
-
-    if submission.over_18 == True:
-        await ctx.send("Sorry but this subreddit is marked as NSFW!")
-    else: 
-        sr_name = random_sub.subreddit
-        author = random_sub.author
-        name = random_sub.title
-        url = random_sub.url
-
-        embed = discord.Embed(title=author, description=name, color=0xFF4500)
-        embed.set_author(name=f'r/{sr_name}',url=url, icon_url='https://i.postimg.cc/pTzSdRqC/reddit-logo.png')
-        embed.set_image(url=url)
-        embed.timestamp = datetime.datetime.utcnow()
-        embed.set_footer(text=f"If the Image is not loading just click on r/{sr_name}!")
-        await ctx.send(embed=embed)
-
-@reddit.error
-async def reddit_error(error, ctx):
-    if isinstance(error, commands.CommandOnCooldown):
-        await ctx.send("Woah Woah you are too fast! Try again in a few seconds", delete_after=5)
 
 
 @client.command()
