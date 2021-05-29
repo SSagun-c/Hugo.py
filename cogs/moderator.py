334193618fab4d29d20b57f4dc13b03b7df45693
Colin <colinpeiffer6@gmail.com>
2021-05-29 11:40:58 +0200

    client?

--- cogs/moderator.py
+++ cogs/moderator.py
@@ -1,23 +1,24 @@
 import discord
 import datetime
+from discord import client
 from discord.ext import commands
 
 class moderatorCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
-    @commands.Cog()
+    @client.command()
     @commands.has_permissions(manage_messages=True)
     async def clear(self, ctx, amount : int):
         await ctx.channel.purge(limit=amount + 1)
 
-    @commands.Cog()
+    @client.command()
     @commands.has_permissions(administrator=True)
     async def kick(self, ctx, member : discord.Member, *, reason=None):
         await member.kick(reason=reason)
         await ctx.send(f'{member} got kicked at {datetime.datetime.utcnow()} because of: {reason}')
 
-    @commands.Cog()
+    @client.command()
     @commands.has_permissions(administrator=True)
     async def ban(self, ctx, member : discord.Member, *, reason=None):
         await ctx.message.delete()
