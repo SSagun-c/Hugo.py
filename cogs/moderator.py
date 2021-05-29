4049d352b8739d6a0410698f289dd0d96309d59b
Colin <colinpeiffer6@gmail.com>
2021-05-29 11:52:27 +0200

    colors

--- cogs/moderator.py
+++ cogs/moderator.py
@@ -13,16 +13,22 @@ class moderatorCog(commands.Cog):
         await ctx.channel.purge(limit=amount + 1)
 
     @commands.command()
-    @commands.has_permissions(administrator=True)
+    @commands.has_permissions(kick_members=True)
     async def kick(self, ctx, member : discord.Member, *, reason=None):
         await member.kick(reason=reason)
-        await ctx.send(f'{member} got kicked at {datetime.datetime.utcnow()} because of: {reason}')
+        await ctx.message.delete()
+        embed = discord.Embed(title="Case: Kick", description=f"User: {member}", color=0xf1c40f)
+        embed.timestamp = datetime.datetime.utcnow()
+        embed.add_field(name="Reason", value=f"{reason}")
+        embed.set_footer(text=f"Kicked by {ctx.message.author}")
+        await ctx.send(embed=embed)
 
     @commands.command()
-    @commands.has_permissions(administrator=True)
+    @commands.has_permissions(ban_members=True)
     async def ban(self, ctx, member : discord.Member, *, reason=None):
         await ctx.message.delete()
-        embed = discord.Embed(title="Case: Ban", description=f"User: {member}")
+        await member.ban(reason=reason)
+        embed = discord.Embed(title="Case: Ban", description=f"User: {member}", color=0xe74c3c)
         embed.timestamp = datetime.datetime.utcnow()
         embed.add_field(name="Reason", value=f"{reason}")
         embed.set_footer(text=f"Banned by {ctx.message.author}")
