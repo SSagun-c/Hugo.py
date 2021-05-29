89eb774ad6fcd7430d3fcee72039308197a04abb
Colin <colinpeiffer6@gmail.com>
2021-05-29 11:37:59 +0200

    hmmm

--- cogs/moderator.py
+++ cogs/moderator.py
@@ -15,7 +15,17 @@ class moderatorCog(commands.Cog):
     @commands.has_permissions(administrator=True)
     async def kick(self, ctx, member : discord.Member, *, reason=None):
         await member.kick(reason=reason)
-        await ctx.send(f'{member.display_name} got kicked at {datetime.datetime.utcnow()} because of: {reason}')
+        await ctx.send(f'{member} got kicked at {datetime.datetime.utcnow()} because of: {reason}')
+
+    @commands.Cog()
+    @commands.has_permissions(administrator=True)
+    async def ban(self, ctx, member : discord.Member, *, reason=None):
+        await ctx.message.delete()
+        embed = discord.Embed(title="Case: Ban", description=f"User: {member}")
+        embed.timestamp = datetime.datetime.utcnow()
+        embed.add_field(name="Reason", value=f"{reason}")
+        embed.set_footer(text=f"Banned by {ctx.message.author}")
+        await ctx.send(embed=embed)
 
 
 
