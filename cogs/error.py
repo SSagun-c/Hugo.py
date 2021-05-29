2aa50f334134362afa8abf0193a5c085c4eb5d50
Colin <colinpeiffer6@gmail.com>
2021-05-29 15:01:26 +0200

    errrrrr

--- cogs/error.py
+++ cogs/error.py
@@ -5,14 +5,15 @@ class errorCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+
     async def on_command_error(self, ctx, exc):
-        if isinstance(exc, MissingPermissions):
+        if isinstance(exc, commands.MissingPermissions):
             await ctx.send(f"Sorry {ctx.message.author.display_name}, you do not meet the required permissions.")
-        elif isinstance(exc, MissingRequiredArgument):
+        elif isinstance(exc, commands.MissingRequiredArgument):
             await ctx.send("One or more required Arguments are missing")
-        elif isinstance(exc, CommandOnCooldown):
+        elif isinstance(exc, commands.CommandOnCooldown):
             await ctx.send(f"That command is on {str(exc.cooldown.type).split('.')[-1]} cooldown. Try again in {exc.retry_after:,.2f} secs.")
-        elif isinstance(exc, NSFWChannelRequired):
+        elif isinstance(exc, commands.NSFWChannelRequired):
             await ctx.send("NSFW Channel is required to use this command")
 
 def setup(bot):
