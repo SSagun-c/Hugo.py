748d409b948e82f73e4805ba21095ae348bb0caf
Colin <colinpeiffer6@gmail.com>
2021-05-31 20:19:42 +0200

    nice

--- main.py
+++ main.py
@@ -23,6 +23,16 @@ async def on_ready():
     change_status.start()
     print("Bot is online and ready to use!")
 
+@client.event
+async def on_command_error(ctx, error):
+            if isinstance(error, commands.MissingPermissions):
+                await ctx.send(f"Sorry {ctx.message.author.display_name}, you do not meet the required permissions.", delete_after=5)
+            elif isinstance(error, commands.MissingRequiredArgument):
+                await ctx.send("One or more required Arguments are missing", delete_after=5)
+            elif isinstance(error, commands.CommandOnCooldown):
+                await ctx.send(f"That command is on cooldown. Try again in {error.retry_after:,.2f} secs.", delete_after=5)
+            elif isinstance(error, commands.NSFWChannelRequired):
+                await ctx.send("NSFW Channel is required to run this command", delete_after=5)
 # Ping
 
 @client.command()
