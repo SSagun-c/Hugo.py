524c8d231dc4663399d284c3c7cfe4c60129b380
SSagun-c <81767182+SSagun-c@users.noreply.github.com>
2021-06-01 11:22:22 +0200

    roleplay.py

--- cogs/roleplay.py
+++ cogs/roleplay.py
@@ -2,7 +2,6 @@ import discord
 import aiohttp
 import datetime
 from typing import Optional
-from discord import Member
 from discord.ext import commands
 from discord.ext.commands import cooldown
 
@@ -13,19 +12,17 @@ class roleplayCog(commands.Cog):
  
   @commands.command()
   @cooldown(1, 5, commands.BucketType.guild)
-  async def cry(self, ctx, target: Optional[Member]):
-    
-    target = target or ctx.message.author
+  async def cry(self, ctx):
     
     async with aiohttp.ClientSession() as session:
       request = await session.get('https://shiro.gg/api/images/cry')
       cryjson = await request.json()
       
     embes = discord.Embed()
-    embed.set_author(name=f"{target.display_name} is crying T_T", url=cryjson['url'])
+    embed.set_author(name=f"{ctx.message.author.display_name} is crying T_T", url=cryjson['url'])
     embed.set_image(url=cryjson['url'])
     embed.timestamp = datetime.datetime.utcnow()
-    embed.set_footer(text=f"Requested by {target.display_name}")
+    embed.set_footer(text=f"Requested by {ctx.message.author.display_name}")
     await ctx.send(embed=embed)
     
     
