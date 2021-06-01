07e5e63ba156cbd42a98422e209a0a48ada410d4
SSagun-c <81767182+SSagun-c@users.noreply.github.com>
2021-06-01 11:05:21 +0200

    roleplay.py

--- cogs/roleplay.py
+++ cogs/roleplay.py
@@ -17,11 +17,11 @@ class roleplayCog(commands.Cog):
     target = target or ctx.message.author
     
     async with aiohttp.ClientSession() as session:
-      request = await session.get('https://shiro.gg/api/images/cry')
+      request = await session.get("https://shiro.gg/api/images/cry")
       cryjson = await request.json()
       
     embes = discord.Embed()
-    embed.set_author(name=f"{target.display_name} is crying :(", url=cryjson['url'])
+    embed.set_author(name=f"{target.display_name} is crying T_T", url=cryjson['url'])
     embed.set_image(url=cryjson['url'])
     embed.timestamp = datetime.datetime.utcnow()
     embed.set_footer(text=f"Requested by {target.display_name"})
