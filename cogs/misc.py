2a547c1a98cec4c2fbbc4ff5e6bf769323090a96
SSagun-c <81767182+SSagun-c@users.noreply.github.com>
2021-06-08 09:41:13 +0200

    update misc.py

--- cogs/misc.py
+++ cogs/misc.py
@@ -173,8 +173,6 @@ class miscCog(commands.Cog):
     @commands.command(case_insensitive = True, aliases = ["remind", "remindme", "remind_me"])
     @commands.bot_has_permissions(attach_files = True, embed_links = True)
     async def reminder(self, ctx, time, *, reminder):
-        print(time)
-        print(reminder)
         user = ctx.message.author
         embed = discord.Embed(color=0x55a7f7, timestamp=datetime.utcnow())
         embed.set_footer(text="If you have any questions, suggestions or bug reports, please join our support Discord Server: link hidden", icon_url=f"{client.user.avatar_url}")
