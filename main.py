70783300024e784f9972e7a2033d08f1c2e8977c
SSagun-c <81767182+SSagun-c@users.noreply.github.com>
2021-06-10 07:50:07 +0200

    update main.py

--- main.py
+++ main.py
@@ -27,13 +27,13 @@ async def on_ready():
 @client.event
 async def on_command_error(ctx, error):
             if isinstance(error, commands.MissingPermissions):
-                await ctx.send(f"Sorry {ctx.message.author.display_name}, you do not meet the required permissions.", delete_after=5)
+                await ctx.send(f"Sorry {ctx.message.author.display_name}, either you or I am missing permissions to do this.")
             elif isinstance(error, commands.MissingRequiredArgument):
-                await ctx.send("One or more required Arguments are missing", delete_after=5)
+                await ctx.send("One or more required Arguments are missing")
             elif isinstance(error, commands.CommandOnCooldown):
-                await ctx.send(f"That command is on cooldown. Try again in {error.retry_after:,.2f} secs.", delete_after=5)
+                await ctx.send(f"That command is on cooldown. Try again in {error.retry_after:,.2f} secs.")
             elif isinstance(error, commands.NSFWChannelRequired):
-                await ctx.send("NSFW Channel is required to run this command", delete_after=5)
+                await ctx.send("NSFW Channel is required to run this command")
 
 
 # Ping
