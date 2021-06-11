dc9ad00de39d5db9cead39c9f05d8bad136ff541
Colin <colinpeiffer6@gmail.com>
2021-06-11 21:40:47 +0200

    Im dying

--- cogs/help.py
+++ cogs/help.py
@@ -12,7 +12,11 @@ class helpCog(commands.Cog):
 
         embed = discord.Embed(title="Currently being edited", description="For help join the help Server [here](https://discord.gg/6JkmzhDsps)\nPrefix  `h!`", color=0x8962AA)
 
-        embed.add_field(name='⚙ General Commands', value="`ping` `8ball` `pussy`\n`serverinfo` `roll` `support`\n`kill` `invite` `repeat`\n`avatar` `userinfo` `wallpaper`")
+        embed.add_field(name='⚙ General Commands', value="`ping` `8ball` `pussy`\n`serverinfo` `roll` `support`\n`kill` `invite` `repeat`\n`avatar` `userinfo` `wallpaper`", inline=True)
+        embed.add_field(name="🎭 Roleplay Commands", value="`kiss` `cry` `hug` `poke`\n`lick` `pat` `nom` `pout`\n`punch` `slap` `blush`\n`smug` `sleep` `tickle`", inline=True)
+        embed.add_field(value="[Invite the bot](https://discord.com/oauth2/authorize?client_id=832922273597227019&permissions=269348086&scope=bot) - [Join the Help Server](https://discord.gg/6JkmzhDsps) - [Vote for me!](https://top.gg/bot/832922273597227019/vote)")
+
+        
         await ctx.send(embed=embed)
 
 def setup(bot):
