c5efb46853a7012af6c4988c58cd8c68e68890f8
SSagun-c <81767182+SSagun-c@users.noreply.github.com>
2021-06-11 22:30:01 +0200

    update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -23,6 +23,24 @@ class helpCog(commands.Cog):
         embed.set_footer(text="Dont know how to use the Moderator commands? Just send h!mod")
 
         await ctx.send(embed=embed)
-
+    
+    
+    @commands.command()
+    async def mod(self, ctx):
+      embed = discord.Embed(title="Info About the Moderation Commands", color=0x738ADB)
+     
+     embed.add_field(name="Ban", value="`h!ban <Member> <Reason(Optional)>")
+     embed.add_field(name="Kick", value="`h!kick <Member> <Reason(Optional)>")
+     embed.add_field(name="Mute", value="`h!mute <time(Leave blank for permamute)> <Reason(Optional)>")
+    embed.add_field(name="Unban", value="`h!unban <User and Discriminator(e.g. h!unban SSagun.py#6969)>")
+    embed.add_field(name="Unmute", value="`h!unmute <member>")
+    embed.add_field(name="Clear", value="`h!clear <amount(Please dont exaggerate)>")
+    
+    embed.set_footer(text="If this still didnt help you, yoy should join the Help Server! h!support")
+    
+    await ctx.send(embed=embed)
+   
+    
+    
 def setup(bot):
     bot.add_cog(helpCog(bot))
