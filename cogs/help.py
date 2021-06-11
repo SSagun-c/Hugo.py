8bedfec8d67d3c398476e9dffa2be94dc9c9b805
Colin <colinpeiffer6@gmail.com>
2021-06-11 21:19:55 +0200

    war

--- cogs/help.py
+++ cogs/help.py
@@ -12,7 +12,7 @@ class helpCog(commands.Cog):
 
         embed = discord.Embed(title="Currently being edited", description="For help join the help Server [here](https://discord.gg/6JkmzhDsps)\nPrefix  `h!`", color=0x8962AA)
 
-        embed.add_field(name='⚙ General Commands', description="`ping` `8ball` `pussy`\n`serverinfo` `roll` `support`\n`kill` `invite` `repeat`\n`avatar` `userinfo` `wallpaper`")
+        embed.add_field(name='General Commands', description="`ping` `8ball` `pussy`\n`serverinfo` `roll` `support`\n`kill` `invite` `repeat`\n`avatar` `userinfo` `wallpaper`")
         await ctx.send(embed=embed)
 
 def setup(bot):
