6604d1b4dbf30329e5e3a45aee816f8d42836527
SSagun-c <81767182+SSagun-c@users.noreply.github.com>
2021-06-11 22:31:41 +0200

    update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -29,7 +29,7 @@ class helpCog(commands.Cog):
     async def mod(self, ctx):
       embed = discord.Embed(title="Info About the Moderation Commands", color=0x738ADB)
      
-     embed.add_field(name="Ban", value="`h!ban <Member> <Reason(Optional)>")
+     embed.add_field(name="Ban", value="`h!ban <Member> <Reason{Optional}>")
      embed.add_field(name="Kick", value="`h!kick <Member> <Reason(Optional)>")
      embed.add_field(name="Mute", value="`h!mute <time(Leave blank for permamute)> <Reason(Optional)>")
     embed.add_field(name="Unban", value="`h!unban <User and Discriminator(e.g. h!unban SSagun.py#6969)>")
