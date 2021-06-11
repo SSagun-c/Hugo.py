3a02c9b110481f37c124fc6e35ab241637fe73d3
SSagun-c <81767182+SSagun-c@users.noreply.github.com>
2021-06-11 22:09:18 +0200

    update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -14,12 +14,14 @@ class helpCog(commands.Cog):
 
         embed.add_field(name='⚙ General Commands', value="`ping` `8ball` `pussy`\n`serverinfo` `roll` `support`\n`kill` `invite` `repeat`\n`avatar` `userinfo` `wallpaper`", inline=True)
         embed.add_field(name="🎭 Roleplay Commands", value="`kiss` `cry` `hug` `poke`\n`lick` `pat` `nom` `pout`\n`punch` `slap` `blush`\n`smug` `sleep` `tickle`", inline=True)
-        embed.add_field(name="🖋 Anime Commands", value="`anime`  `neko` `animeweb`", inline=False)
+        embed.add_field(name="🖋 Anime Commands", value="`anime`  `neko` `animeweb`", inline=True)
         embed.add_field(name="🔞 NSFW Commands", value="`hentai` `trap` `thighs`\n`boobs` `yuri`", inline=True)
-        embed.add_field(name=f"{reddit} Reddit Command", value="`reddit <your subreddit here>`", inline=False)
+        embed.add_field(name=f"{reddit} Reddit Command", value="`reddit <your subreddit here>`", inline=True)
+        embed.add_field(name="🔎 Moderator Commands", value="`ban` `kick` `clear`\n`mute`", inline=True)
         embed.add_field(name="About the Bot", value="[Invite the bot](https://discord.com/oauth2/authorize?client_id=832922273597227019&permissions=269348086&scope=bot) - [Join the Help Server](https://discord.gg/6JkmzhDsps) - [Vote for me!](https://top.gg/bot/832922273597227019/vote)", inline=False)
-
         
+        embed.set_footer(text="Dont know how to use the Moderator commands? Just send h!mod")
+
         await ctx.send(embed=embed)
 
 def setup(bot):
