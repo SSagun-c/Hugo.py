f93a7addf39b8bd498c024b1225f0556673ae336
Colin <colinpeiffer6@gmail.com>
2021-06-12 12:04:00 +0200

    Better

--- cogs/help.py
+++ cogs/help.py
@@ -29,12 +29,12 @@ class helpCog(commands.Cog):
     async def mod(self, ctx):
       embed = discord.Embed(title="Info About the Moderation Commands", color=0x738ADB)
      
-      embed.add_field(name="Ban", value="`h!ban <Member> <Reason(Optional)>`")
-      embed.add_field(name="Kick", value="`h!kick <Member> <Reason(Optional)>`")
-      embed.add_field(name="Mute", value="`h!mute <time(Leave blank for permamute)> <Reason(Optional)>`")
-      embed.add_field(name="Unban", value="`h!unban <User and Discriminator(e.g. h!unban SSagun.py#6969)>`")
-      embed.add_field(name="Unmute", value="`h!unmute <member>`")
-      embed.add_field(name="Clear", value="`h!clear <amount(Please dont exaggerate)>`")
+      embed.add_field(name="Ban", value="`h!ban <Member> <Reason(Optional)>`", inline=False)
+      embed.add_field(name="Kick", value="`h!kick <Member> <Reason(Optional)>`", inline=False)
+      embed.add_field(name="Mute", value="`h!mute <time(Leave blank for permamute)> <Reason(Optional)>`", inline=False)
+      embed.add_field(name="Unban", value="`h!unban <User and Discriminator(e.g. h!unban SSagun.py#6969)>`", inline=False)
+      embed.add_field(name="Unmute", value="`h!unmute <member>`", inline=False)
+      embed.add_field(name="Clear", value="`h!clear <amount(Please dont exaggerate)>`", inline=False)
         
       embed.set_footer(text="If this still didnt help you, you should join the Help Server! h!support")
       
