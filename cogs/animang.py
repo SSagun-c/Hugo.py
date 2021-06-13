a02e3df7955e120c64541c0554a23de5317d0482
Colin <colinpeiffer6@gmail.com>
2021-06-13 20:15:27 +0200

    ok

--- cogs/animang.py
+++ cogs/animang.py
@@ -4,7 +4,7 @@ import kitsu
 from discord.ext import commands
 
 
-kit = kitsu.Client()
+client = kitsu.Client()
 
 class animangCog(commands.Cog):
     def __init__(self, bot):
@@ -12,39 +12,57 @@ class animangCog(commands.Cog):
 
     
     @commands.command()
-    async def anime(self, ctx, query):
-        entries = await kit.search('anime', query, limit=1)
+    async def anime(self, ctx, *, aniname):
+        entries = await client.search('anime', aniname)
         if not entries:
             embed = discord.Embed(title="❌ Nothing Found!")
             await ctx.send(embed=embed)
-            return
         
-        for i, anime in enumerate(entries, 1):
-            embed = discord.Embed(title=f"Results for {anime.title}")
-            fields = ['Sub-Type', anime.subtype, False,
-                    'Status', anime.status, False,
-                    'Sypnosis', anime.sypnosis, False,
-                    'Episodes', anime.episode_count, False,
-                    'Age Rating', anime.age_rating_guide, False,
-                    'Popularity', anime.popularity_rank, False,
-                    'Rating', anime.rating_rank, False]
+        else:
+        
+            for anime in enumerate(entries, 1):
+                embed = discord.Embed(title=f"Results for {anime.title}")
+                fields = [
+                            'Sub-Type', anime.subtype, False,
 
-            for name, value, inline in fields:
-                embed.add_field(name=name, value=value, inline=inline)
+                            'Status', anime.status, False,
 
-        if anime.started_at:
-            embed.add_field(name='Started', value=anime.started_at.strftime('%Y/%m/%d'))
-        if anime.ended_at:
-            embed.add_field(name="Ended", value=anime.ended_at.strftime('%Y/%m/%d'))
+                            'Sypnosis', anime.sypnosis, False,
 
-        streaming_links = await kit.fetch_anime_streaming_links(anime)
-        if streaming_links:
-            for link in streaming_links:
-                embed.add_field(name="Streaming Links", value=f'[{link.title}]({link.url})')
+                            'Episodes', anime.episode_count, False,
 
-        embed.add_field(name="Kitsu Page", value=anime.url)
+                            'Age Rating', anime.age_rating_guide, False,
 
-        await ctx.send(embed=embed)
+                            'Popularity', anime.popularity_rank, False,
+
+                            'Rating', anime.rating_rank, False  
+                        
+                        ]
+
+
+                for name, value, inline in fields:
+                    embed.add_field(name=name, value=value, inline=inline)
+
+            if anime.started_at:
+
+                embed.add_field(name='Started', value=anime.started_at.strftime('%Y/%m/%d'))
+
+            if anime.ended_at:
+
+                embed.add_field(name="Ended", value=anime.ended_at.strftime('%Y/%m/%d'))
+                
+
+            streaming_links = await client.fetch_anime_streaming_links(anime)
+
+            if streaming_links:
+
+                for link in streaming_links:
+
+                    embed.add_field(name="Streaming Links", value=f'[{link.title}]({link.url})')
+
+            embed.add_field(name="Kitsu Page", value=anime.url)
+
+            await ctx.send(embed=embed)
 
                 
 def setup(bot):
