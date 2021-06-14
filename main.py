6eb090067b9cc1cc7e626d894212b83cacb218da
Colin <colinpeiffer6@gmail.com>
2021-06-14 18:47:02 +0200

    And we go

--- main.py
+++ main.py
@@ -5,15 +5,21 @@ import topgg
 from discord.ext import commands, tasks
 from discord import Intents
 from itertools import cycle
+from discord.ext.commands import when_mentioned_or
 
-
+PREFIX = 'h!'
 topggtoken = os.getenv("TOPGGTOKEN")
 token = os.getenv("DISCORD_TOKEN")
-client = commands.Bot(command_prefix='h!', intents=Intents.all())
+client = commands.Bot(command_prefix=PREFIX, intents=Intents.all())
 client.remove_command('help')
 
 dbl_token = topggtoken
 client.topgg = topgg.DBLClient(client, dbl_token)
+
+
+def get_prefix(bot, message):
+    return when_mentioned_or(PREFIX)(bot, message)
+
 
 # Events
 
