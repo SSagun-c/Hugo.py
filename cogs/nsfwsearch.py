76a5c72d4b48d221bb8074a2261dbbf518a2122d
Colin <colinpeiffer6@gmail.com>
2021-06-29 21:43:48 +0200

    r34

--- cogs/nsfwsearch.py
+++ cogs/nsfwsearch.py
@@ -14,7 +14,17 @@ class nsfwCog(commands.Cog):
     @commands.is_nsfw()
     async def hentai_haven(self, ctx, *args):
         video = await hh.category_search(str(" ").join(args))
-        await ctx.send(video)
+
+        embed = discord.Embed(title=f"{ctx.message.author.display_name} Searched for {args}", description=f"Link: {video}", color=0xDB3B5E)
+        
+        await ctx.send(embed=embed)#
+
+    
+    @commands.command(aliases=['r34'])
+    @commands.is_nsfw()
+    async def rule34(self, ctx, *args):
+        image = await r34.image_search(str(" ").join(args))
+        await ctx.send(image)
 
 
 def setup(bot):
