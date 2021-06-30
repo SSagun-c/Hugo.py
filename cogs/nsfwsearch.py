d3407f1cc59cc49bf2b3516eae5805925c1c145d
Colin <colinpeiffer6@gmail.com>
2021-06-30 17:54:59 +0200

    Tets

--- cogs/nsfwsearch.py
+++ cogs/nsfwsearch.py
@@ -3,8 +3,6 @@ import mediascraper
 from discord.ext import commands
 
 hh = mediascraper.HentaiHaven()
-r34 = mediascraper.Rule34()
-gb = mediascraper.Gelbooru()
 
 class nsfwCog(commands.Cog):
     def __init__(self, bot):
@@ -16,20 +14,6 @@ class nsfwCog(commands.Cog):
     async def hentai_haven(self, ctx, *args):
         video = await hh.category_search(str(" ").join(args))
         await ctx.send(video)
-
-    
-    @commands.command(aliases=['r34'])
-    @commands.is_nsfw()
-    async def rule34(self, ctx, *args):
-        image = await r34.image_search(str(" ").join(args))
-        await ctx.send(image)
-
-
-    @commands.command(aliases=['gbooru', 'gb'])
-    @commands.is_nsfw()
-    async def gelbooru(self, ctx, *args):
-        image = await gb.image_search(str(" ").join(args))
-        await ctx.send(image)
 
 
 def setup(bot):
