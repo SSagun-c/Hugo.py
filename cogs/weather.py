040bce1c39f1c2b0f45c1c90a9e1a2c70dcbda93
Colin <colinpeiffer6@gmail.com>
2021-07-03 00:18:03 +0200

    await sorry

--- cogs/weather.py
+++ cogs/weather.py
@@ -15,12 +15,10 @@ class weatherCog(commands.Cog):
     @cooldown(1, 10, commands.BucketType.user)
     async def weather_m(self, ctx, *, city_name):
         async with aiohttp.ClientSession() as session:
-            target = ctx.message.author
-
             request = await session.get(f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid=08c3bc9d0b3e4eb229d9a271bebb05ef&units=metric")
-            weatherjson = request.json()
+            weatherjson = await request.json()
 
-        embed = discord.Embed(title=weatherjson['name'], description=f"{weatherjson['weather'][0]['description']}\nLocated in: {weatherjson['sys']['country']}", colour=target.colour)
+        embed = discord.Embed(title=weatherjson['name'], description=f"{weatherjson['weather'][0]['description']}\nLocated in: {weatherjson['sys']['country']}", colour=0xEEEFFF)
         embed.set_thumbnail(url=f"https://openweathermap.org/img/wn/{weatherjson['weather'][0]['icon']}@2x.png")
         embed.add_field(name=f"Temperature {weatherjson['main']['temp']}°C", value=f"Feels Like {weatherjson['main']['feels_like']}°C\nMinimum Temperature {weatherjson['main']['temp_min']}°C\nMaximum Temperature {weatherjson['main']['temp_max']}°C\nPressure {weatherjson['main']['pressure']}PA\nHumidity {weatherjson['main']['humidity']}%", inline=False)
         embed.add_field(name="Wind", value=f"Speeds {weatherjson['wind']['speed']}kmh", inline=False)
@@ -31,14 +29,12 @@ class weatherCog(commands.Cog):
 
     @commands.command(name="weather -imperial", aliases=['weather -imp', 'weather -i'])
     @cooldown(1, 10, commands.BucketType.user)
-    async def weather_m(self, ctx, *, city_name):
+    async def weather_i(self, ctx, *, city_name):
         async with aiohttp.ClientSession() as session:
-            target = ctx.message.author
-
             request = await session.get(f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid=08c3bc9d0b3e4eb229d9a271bebb05ef&units=metric")
-            weatherjson = request.json()
+            weatherjson = await request.json()
 
-        embed = discord.Embed(title=weatherjson['name'], description=f"{weatherjson['weather'][0]['description']}\nLocated in: {weatherjson['sys']['country']}", colour=target.colour)
+        embed = discord.Embed(title=weatherjson['name'], description=f"{weatherjson['weather'][0]['description']}\nLocated in: {weatherjson['sys']['country']}", colour=0xFFFEEE)
         embed.set_thumbnail(url=f"https://openweathermap.org/img/wn/{weatherjson['weather'][0]['icon']}@2x.png")
         embed.add_field(name=f"Temperature {weatherjson['main']['temp']}°F", value=f"Feels Like {weatherjson['main']['feels_like']}°F\nMinimum Temperature {weatherjson['main']['temp_min']}°F\nMaximum Temperature {weatherjson['main']['temp_max']}°F\nPressure {weatherjson['main']['pressure']}PA\nHumidity {weatherjson['main']['humidity']}%", inline=False)
         embed.add_field(name="Wind", value=f"Speeds {weatherjson['wind']['speed']}mph", inline=False)
