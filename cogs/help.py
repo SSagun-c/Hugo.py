3c8b1e1f20d26c16b78115917baecb4b7de8b44a
SSagun-c <81767182+SSagun-c@users.noreply.github.com>
2021-07-07 23:40:59 +0200

    update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -12,13 +12,13 @@ class helpCog(commands.Cog):
         reddit = '<:reddit:853000371601277018>'
         embed = discord.Embed(title="Hugo.py Commands", description="For help join the help Server [here](https://discord.gg/6JkmzhDsps)", color=0x8962AA)
 
-        embed.add_field(name='🥳 Misc Commands', value="`ping`  `8ball`  `pussy`\n`serverinfo`  `roll`  `support`\n`kill`  `invite`  `repeat`\n`avatar`  `userinfo`  `wallpaper`\n`botinfo`  `wouldyourather`\n`weather (shows in metric) <city>`\n`iweather (shows in imperial) <city>`", inline=False)
+        embed.add_field(name='🥳 Misc Commands', value="`ping`  `8ball`  `pussy`  `serverinfo`  `roll`  `support`  `kill`  `invite`  `repeat`  `avatar`  `userinfo`  `wallpaper`  `botinfo`  `wouldyourather`  `weather (shows in metric) <city>`  `iweather (shows in imperial) <city>`", inline=False)
         embed.add_field(name="📸 Image Manipulation", value="`simp`  `license`", inline=False)
-        embed.add_field(name="🎭 Roleplay Commands", value="`kiss`  `cry`  `hug`  `poke`\n`lick`  `pat`  `nom`  `pout`\n`punch`  `slap`  `blush`\n`smug`  `sleep`  `tickle`", inline=False)
+        embed.add_field(name="🎭 Roleplay Commands", value="`kiss`  `cry`  `hug`  `poke`  `lick`  `pat`  `nom`  `pout`  `punch`  `slap`  `blush`  `smug`  `sleep`  `tickle`", inline=False)
         embed.add_field(name="🖋 Anime Commands", value="`waifu`  `neko`  `animeweb`  `anime <Anime Name>`  `manga <Manga Name>`", inline=False)
-        embed.add_field(name="🔞 NSFW Commands", value="`hentai`  `trap`  `thighs`\n`boobs`  `yuri` `bondage`", inline=False)
+        embed.add_field(name="🔞 NSFW Commands", value="`hentai`  `trap`  `thighs`  `boobs`  `yuri` `bondage`  `bdsm`  `ass`  `cum`  `tentacles`  `blowjob`  `masturbation`  `ero`  `ahegao`  `gif`" , inline=False)
         embed.add_field(name=f"{reddit} Reddit Command", value="`reddit <your subreddit here>`", inline=False)
-        embed.add_field(name="🔎 Moderator Commands", value="`ban`  `kick`  `clear`\n`mute`  `unmute`  `unban`  `prefix`  `clearprefix`", inline=False)
+        embed.add_field(name="🔎 Moderator Commands", value="`ban`  `kick`  `clear`  `mute`  `unmute`  `unban`  `prefix`  `clearprefix`", inline=False)
         embed.add_field(name="About the Bot", value="**Report bugs**\n`report`\n\n[Invite the bot](https://discord.com/oauth2/authorize?client_id=832922273597227019&permissions=269348086&scope=bot) - [Join the Help Server](https://discord.gg/6JkmzhDsps) - [Vote for me!](https://top.gg/bot/832922273597227019/vote)", inline=False)
         
         embed.set_footer(text="Dont know how to use the Moderator commands? Just send h!mod")
